ad0ab0be4f93decb717f431436518c8771d03358
rand0mgh <65503947+rand0mgh@users.noreply.github.com>
2021-11-21 14:59:57 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
         <p className="description">
           dont ask why i registered <code>merp.ga</code>
         </p>
+    <blockquote class="twitter-tweet" data-theme="dark"><p lang="und" dir="ltr"><a href="https://t.co/gU5KYTqDpa">https://t.co/gU5KYTqDpa</a> <a href="https://t.co/4G0DGNjrGo">pic.twitter.com/4G0DGNjrGo</a></p>&mdash; Tytolis (@Tytolisss) <a href="https://twitter.com/Tytolisss/status/1455707549194330116?ref_src=twsrc%5Etfw">November 3, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </main>
 
       <Footer />
