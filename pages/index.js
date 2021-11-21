73279e626f749f84cdacc8d4ffbf3bba89445eb2
rand0mgh <65503947+rand0mgh@users.noreply.github.com>
2021-11-21 13:28:33 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>merp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="merp" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          dont ask why i registered <code>merp.ga</code>
         </p>
       </main>
 
