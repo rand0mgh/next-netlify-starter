44bd5f58a15e0d2ab2b03b8f9d661d3688be0f81
rand0mgh <65503947+rand0mgh@users.noreply.github.com>
2021-11-21 15:07:18 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <p className="description">
           dont ask why i registered <code>merp.ga</code>
         </p>
-    <blockquote class="twitter-tweet" data-dnt="true"><p lang="en" dir="ltr">Welcome to the 🅱️etaverse. <a href="https://t.co/3MbP8FKXfd">pic.twitter.com/3MbP8FKXfd</a></p>&mdash; Sig! 🔜 MFF, FC, FWA (@sig_serg) <a href="https://twitter.com/sig_serg/status/1455555611719045126?ref_src=twsrc%5Etfw">November 2, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/YiVReYMaEoY?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <p className="description">
           Welcome to the Fetaverse.</code>
         </p>
