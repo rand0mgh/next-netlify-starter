fa274505d517210b4bb609743f6cad168c3a881d
rand0mgh <65503947+rand0mgh@users.noreply.github.com>
2021-11-22 09:06:41 +0000

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        srgl
+        srgl (<code>powered by netlify</code>)
       </footer>
     </>
   )
